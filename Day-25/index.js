d0eca3b828cdd8b4408aebeaf3dd46ee85a5bc64
Kanak Yadav <yadavkanak109@gmail.com>
2025-11-13 20:54:54 +0530

    Day 25 done

---
+++ Day-25/index.js
@@ -0,0 +1,24 @@
+
+
+let arr = [2,3,4,5,6]
+
+arr.forEach(function(val){
+  console.log(val);
+  
+})
+
+
+let arr1 = [2,3,4,5,6];
+
+console.log("\n",arr1[1]);
+
+
+
+let obj = {
+    name :"kanak Yadav",
+    age  : 20,
+    rollno : "03515102024",
+    collage : "GGSIPU"
+}
+
+console.log(obj)
