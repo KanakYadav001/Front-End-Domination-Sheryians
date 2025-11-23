c17623192e4635938a11d9f08cc2761d4aa32994
Kanak Yadav <yadavkanak109@gmail.com>
2025-11-24 00:17:46 +0530

    question 2 done

--- DOM Practic/index.js
+++ DOM Practic/index.js
@@ -4,3 +4,17 @@ let para = document.querySelector('p')
 btn.addEventListener('click',function () {
     para.textContent = "helooo DOM"
 })
+
+
+let img1 = document.querySelector('.img1')
+let img2 = document.querySelector('.img2')
+let btn2 = document.querySelector('.btn2')
+
+btn2.addEventListener('click', function() {
+    let im1 = img1.src
+    let im2 = img2.src
+
+    img1.src = im2
+    img2.src = im1
+
+})
