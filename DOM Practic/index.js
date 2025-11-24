0f64e48b5786ac6896b841a3e9f99701daf783ea
Kanak Yadav <yadavkanak109@gmail.com>
2025-11-24 22:28:54 +0530

    q3 done

--- DOM Practic/index.js
+++ DOM Practic/index.js
@@ -1,20 +1,40 @@
 
-let btn =document.querySelector('button')
-let para = document.querySelector('p')
-btn.addEventListener('click',function () {
-    para.textContent = "helooo DOM"
-})
+// let btn =document.querySelector('button')
+// let para = document.querySelector('p')
+// btn.addEventListener('click',function () {
+//     para.textContent = "helooo DOM"
+// })
 
 
-let img1 = document.querySelector('.img1')
-let img2 = document.querySelector('.img2')
-let btn2 = document.querySelector('.btn2')
+// let img1 = document.querySelector('.img1')
+// let img2 = document.querySelector('.img2')
+// let btn2 = document.querySelector('.btn2')
 
-btn2.addEventListener('click', function() {
-    let im1 = img1.src
-    let im2 = img2.src
+// btn2.addEventListener('click', function() {
+//     let im1 = img1.src
+//     let im2 = img2.src
 
-    img1.src = im2
-    img2.src = im1
+//     img1.src = im2
+//     img2.src = im1
 
+// })
+
+
+let form  = document.querySelector('form')
+let input = document.querySelectorAll('input[type="text"]')
+let p = document.querySelector('p')
+
+
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault()
+    for(let i=0 ; i<input.length ; i++) {
+        if(input[i].value.trim() === ''){
+            p.textContent = "Please Fill The Filds First",
+            p.style.color = "red"
+        }
+        else { 
+            p.textContent = ""
+        }
+    }
 })
