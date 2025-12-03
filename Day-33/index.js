fe867182a271bb79d20bb01b51b692e1a53742cf
Kanak Yadav <yadavkanak109@gmail.com>
2025-12-04 01:06:06 +0530

    day 33 done

---
+++ Day-33/index.js
@@ -0,0 +1,62 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+
+
+
+
+let h1ele = document.querySelectorAll('.page2 h1')
+
+h1ele.forEach(function(ele) {
+    let clutter = ""
+  let h1data = ele.textContent
+  let splitContent = h1data.split("")
+
+  splitContent.forEach(function(e) {
+     clutter += `<span>${e}</span>`
+  })
+
+  ele.innerHTML = clutter
+})
+
+
+
+gsap.to(".page2  h1 span", {
+    color :"#dadada",
+    stagger :0.1 ,
+    scrollTrigger : {
+        trigger : ".page2 h1",
+        scroller : ".main",
+        markers : true,
+        start : "top 40%",
+        end : "top -20%",
+        scrub : 1
+    }
+} )
